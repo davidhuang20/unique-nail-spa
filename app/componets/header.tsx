5483dc9c2a4471ddd0fa3ff55539c904b33a1ede
Zhengda-Huang <huang.zhen@northeastern.edu>
2023-12-27 15:34:40 -0500

    fix

--- app/componets/header.tsx
+++ app/componets/header.tsx
@@ -2,7 +2,7 @@
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import Image from 'next/image'
-import { Button, BookButton, Nav } from '.';
+import { Button, Nav } from '.';
 
 
 const Header = () => {
