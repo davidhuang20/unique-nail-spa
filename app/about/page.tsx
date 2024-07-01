defe4a42c9b4515efa372af18776f9104aef0025
Zhengda-Huang <huang.zhen@northeastern.edu>
2024-07-01 19:25:30 -0400

    modify about us

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import Image from 'next/image'
-import { interior, journyPic } from '../../constant/constant'
+import { galleryPic, interior, journyPic } from '../../constant/constant'
 import { PhotoList } from '../componets'
+import Link from 'next/link'
+import '../style/gallery.css';
 
 const Page = () => {
+
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const handleImageClick = (image: string) => { // Specify the type for the image parameter
+    setSelectedImage(image);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedImage(null);
+  };
   return (
     <div className='px-4 sm:px-24'>
       <section className='md:h-1/8 py-6 md:py-16 text-center text-3xl sm:text-4xl md:text-5xl lg:text-6xl px-12 flex items-center justify-center'>
@@ -41,9 +54,47 @@ const Page = () => {
             ))}
         </div>
       </section>
-      <section className='nail-fashion'>
-        <h1 className='text-center py-6 md:py-24 text-4xl'>Our Latest Work</h1>
-        <PhotoList pictures={journyPic}/>
+      <section className='py-24 px-3 sm:px-24'>
+        <h1 className='text-center md:py-24 text-4xl py-6'>Our Latest Work</h1>
+
+        <div className="main">
+        {galleryPic.map((image, index) => (
+          <div key={index} onClick={() => handleImageClick(image)}>
+            <Image
+              src={image}
+              alt="Image"
+              width={400}
+              height={300}
+              className="hover:scale-110 transition ease-in-out delay-150"
+            />
+          </div>
+        ))}
+
+        {selectedImage && (
+          <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-50">
+            <div className="bg-white p-8 rounded shadow-lg">
+              <Image
+                src={selectedImage}
+                alt="Selected Image"
+                className="modal-image"
+                width={400}
+                height={300}
+              />
+              <div className="flex justify-end">
+                <button
+                  className="px-4 py-2 bg-black text-white rounded hover:bg-white hover:text-black hover:border hover:border-black md:text-sm text-base"
+                  onClick={handleCloseModal}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className='text-xl sm:text-2xl md:text-3xl lg:text-4xl text-center pt-12'>
+            Join this journey with us on Instagram <Link href="https://www.instagram.com/uniquenail_brightwater/" target='_blank'> <a className='underline'>@uniquenail_brightwater</a></Link>
+        </div>
       </section>
     </div>
   )
